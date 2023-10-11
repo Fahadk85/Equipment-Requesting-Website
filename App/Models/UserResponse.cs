3dd82569949aeb100b7fba43365f7b73a1bac255
Fahadk85 <60100766+Fahadk85@users.noreply.github.com>
2023-10-11 13:07:06 -0400

    Added all the necessary fields into the UserResponse Model

---
+++ App/Models/UserResponse.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models
+{
+    public class UserResponse
+    {
+        [Required]
+        public string Name {get; set;}
+
+        [Required]
+        [EmailAddress]
+        public string Email {get; set;}
+
+        [Required]
+        [Phone]
+        public string PhoneNumber {get; set;}
+
+        [Required]
+        public string UserType {get; set;}
+
+        [Required]
+        public string EquipmentType {get; set;}
+
+        [Required]
+        public string RequestDetails {get; set;}
+
+        [Required]
+        public string Duration {get; set;}
+        
+    }
+}
